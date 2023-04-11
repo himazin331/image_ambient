e92f1380afa5d35032cb1a1b19cd3075617b1b3a
himazin331 <sion20022013@gmail.com>
2023-04-12 02:33:22 +0900

    Added rounding process for image size

--- src/ambient.py
+++ src/ambient.py
@@ -1,71 +1,69 @@
 import cv2
+import argparse as arg
 import numpy as np
 
-DISPLAY_WIDTH = 1920
-DISPLAY_HEIGHT = 1080
-GAUSS_SIGMA = 200
-ALPHA_DIST = 50.0
+parser = arg.ArgumentParser(description="Ambient Image Generator")
+parser.add_argument("--img_path", "-i", type=str, default=None, help="Input image path (PNG Only)")
+parser.add_argument("--output_path", "-o", type=str, default="output.png", help="Output image path")
+parser.add_argument("--disp_width", "-dw", type=int, default=1920, help="Display width (default: 1920)")
+parser.add_argument("--disp_height", "-dh", type=int, default=1080, help="Display height (default: 1080)")
+parser.add_argument("--gauss_sigma", "-gs", type=int, default=200, help="Gaussian blur sigma (default: 200)")
+parser.add_argument("--alpha_dist", "-ad", type=float, default=50.0, help="Distance of transparent gradient of image border (default: 50.0)")
+args = parser.parse_args()
 
-IMG_PATH = "test_img3.png"
-OUTPUT_PATH = "output3.png"
+print(f"DISPLAY SIZE: {args.disp_width} x {args.disp_height}")
+print("ALPHA_DIST: ", args.alpha_dist)
+print("GAUSS_SIGMA: ", args.gauss_sigma)
+print("INPUT IMAGE PATH: ", args.img_path)
+print("OUTPUT IMAGE PATH: ", args.output_path)
 
-# TODO argparseでコマンド引数取れるようにする
-
-print(f"DISPLAY SIZE: {DISPLAY_WIDTH} x {DISPLAY_HEIGHT}")
-print("ALPHA_DIST: ", ALPHA_DIST)
-print("GAUSS_SIGMA: ", GAUSS_SIGMA)
-print("INPUT IMAGE PATH: ", IMG_PATH)
-print("OUTPUT IMAGE PATH: ", OUTPUT_PATH)
-
-org_img = cv2.imread(IMG_PATH)
-
-# org_img = cv2.resize(org_img, (int(DISPLAY_WIDTH * 0.9), int(DISPLAY_HEIGHT * 0.9)))
+org_img = cv2.imread(args.img_path)
 height, width, _ = org_img.shape
 print(f"INPUT IMAGE SIZE: {width} x {height}")
+
+pad_width: int = int((args.disp_width - width) / 2)
+pad_height: int = int((args.disp_height - height) / 2)
+# * Rounding if image size exceeds display size
+if pad_width < 0 or pad_height < 0:
+    print("Image size is too large. Resizing...")
+    if pad_width < 0:
+        aspect: float = height / width
+        re_height: int = int(args.disp_width * aspect)
+        org_img = cv2.resize(org_img, (args.disp_width, re_height))
+        pad_width = 0
+    if pad_height < 0:
+        aspect: float = width / height
+        re_width: int = int(args.disp_height * aspect)
+        org_img = cv2.resize(org_img, (re_width, args.disp_height))
+        pad_height = 0
+    height, width, _ = org_img.shape
+    print(f"RESIZED IMAGE SIZE: {width} x {height}")
+    pad_width = int((args.disp_width - width) / 2)
+    pad_height = int((args.disp_height - height) / 2)
 
 # * Create a transparent gradient
 alpha_channels = np.zeros((height, width), dtype=np.uint16)
 a_height, a_width = alpha_channels.shape
-# ! too slow
 for y in range(a_height):
     for x in range(a_width):
-        alpha_channels[y, x] = int(255 * min(min(x, y), min(a_width - x, a_height - y)) / ALPHA_DIST)
+        alpha_channels[y, x] = int(255 * min(min(x, y), min(a_width - x, a_height - y)) / args.alpha_dist)
 alpha_channels = np.clip(alpha_channels, 0.0, 255.0)
 # add alpha channel
 add_alpha_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2BGRA)
 add_alpha_img[:, :, 3] = alpha_channels
 
 # * Padding a blur effect background
-# calculation of padding area
-pad_width = int((DISPLAY_WIDTH - width) / 2)
-pad_height = int((DISPLAY_HEIGHT - height) / 2)
-pad_width = pad_width if pad_width > 0 else 0
-pad_height = pad_height if pad_height > 0 else 0
-
-# TODO 画像サイズがディスプレイサイズを超過している場合は丸め込む処理をいれる
-# re_height = 0
-# re_width = 0
-# if pad_width < 0:
-#     aspect = width / height
-#     re_height = int(1080 * aspect)
-#     pad_width = 0
-# if pad_height < 0:
-#     aspect = height / width
-#     re_width = int(1920 * aspect)
-#     pad_height = 0
-
-# if re_height > 0:
-#     org_img = cv2.resize(org_img, (re_height, 1080))
-# if re_width > 0:
-#     org_img = cv2.resize(org_img, (1920, re_width))
-
 # add padding to image
 padding_img = cv2.copyMakeBorder(org_img, pad_height, pad_height, pad_width, pad_width, cv2.BORDER_REFLECT)
-bler_img = cv2.GaussianBlur(padding_img, (0, 0), GAUSS_SIGMA)  # ! too slow
-
+padding_img = cv2.resize(padding_img, (args.disp_width, args.disp_height))  # Completely fill in just a few shortfalls.
+bler_img = cv2.GaussianBlur(padding_img, (0, 0), args.gauss_sigma)  # ! too slow
+# overwrite image
 x1, y1, x2, y2 = pad_width, pad_height, width + pad_width, height + pad_height,
 bler_img[y1:y2, x1:x2] = bler_img[y1:y2, x1:x2] * (1 - add_alpha_img[:, :, 3:] / 255) + add_alpha_img[:, :, :3] * (add_alpha_img[:, :, 3:] / 255)
 
+height, width, _ = bler_img.shape
+print(f"OUTPUT IMAGE SIZE: {width} x {height}")
+
 # cv2.imshow("preview", bler_img)
 # cv2.waitKey(0)
-cv2.imwrite(OUTPUT_PATH, bler_img)
+cv2.imwrite(args.output_path, bler_img)
